4da746a5babf6d3745144b7fced0564cd1ca855a
Shantel1999 <shantellawrence79@gmail.com>
2022-09-12 18:54:37 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <studio.h>
 
 /**
- * main - print
- *
- * Description - using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
- */
+* main - print
+*
+* Description - using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
